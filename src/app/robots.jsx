53c168618083e136f612d9daf79c303621328cda
tasesmuemils <emils.bisenieks93@gmail.com>
2025-01-30 14:32:02 +0200

    Few edits

--- src/app/robots.jsx
+++ src/app/robots.jsx
@@ -1,6 +1,14 @@
 export default function robots() {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
   return {
-    rules: [{ userAgent: '*', allow: '/' }],
-    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/api/', '/_next/', '/_vercel/'],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
   };
 }
