c6b96d86df81adccf6a6db8daa561d153e139c20
tasesmuemils <emils.bisenieks93@gmail.com>
2025-01-31 14:14:26 +0200

    Sitemap updated

--- src/app/sitemap.jsx
+++ src/app/sitemap.jsx
@@ -1,27 +1,55 @@
+// export default async function sitemap() {
+//   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+//   return [
+//     // Default (Latvian) Pages
+//     { url: `${baseUrl}/lv` },
+//     { url: `${baseUrl}/lv/about` },
+//     { url: `${baseUrl}/lv/services` },
+//     { url: `${baseUrl}/lv/video` },
+//     { url: `${baseUrl}/lv/digital` },
+//     { url: `${baseUrl}/lv/contact` },
+//     { url: `${baseUrl}/lv/maket` },
+//     { url: `${baseUrl}/lv/modeling` },
+//     { url: `${baseUrl}/lv/cookies` },
+
+//     // English Pages
+//     { url: `${baseUrl}/en` },
+//     { url: `${baseUrl}/en/about` },
+//     { url: `${baseUrl}/en/services` },
+//     { url: `${baseUrl}/en/video` },
+//     { url: `${baseUrl}/en/digital` },
+//     { url: `${baseUrl}/en/contact` },
+//     { url: `${baseUrl}/en/maket` },
+//     { url: `${baseUrl}/en/modeling` },
+//     { url: `${baseUrl}/en/cookies` },
+//   ];
+// }
+
 export default async function sitemap() {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-
-  return [
-    // Default (Latvian) Pages
-    { url: `${baseUrl}` },
-    { url: `${baseUrl}/about` },
-    { url: `${baseUrl}/services` },
-    { url: `${baseUrl}/video` },
-    { url: `${baseUrl}/digital` },
-    { url: `${baseUrl}/contact` },
-    { url: `${baseUrl}/maket` },
-    { url: `${baseUrl}/modeling` },
-    { url: `${baseUrl}/cookies` },
-
-    // English Pages
-    { url: `${baseUrl}/en` },
-    { url: `${baseUrl}/en/about` },
-    { url: `${baseUrl}/en/services` },
-    { url: `${baseUrl}/en/video` },
-    { url: `${baseUrl}/en/digital` },
-    { url: `${baseUrl}/en/contact` },
-    { url: `${baseUrl}/en/maket` },
-    { url: `${baseUrl}/en/modeling` },
-    { url: `${baseUrl}/en/cookies` },
+  const pages = [
+    '',
+    'about',
+    'services',
+    'video',
+    'digital',
+    'contact',
+    'maket',
+    'modeling',
+    'cookies',
   ];
+
+  const locales = ['lv', 'en'];
+
+  const urls = locales.flatMap((locale) =>
+    pages.map((page) => ({
+      loc: `${baseUrl}/${locale}${page ? `/${page}` : ''}`,
+      lastModified: new Date().toISOString(),
+      changefreq: 'weekly',
+      priority: 0.8,
+    }))
+  );
+
+  return urls;
 }
